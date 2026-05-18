ae29735f087dd5dd21f42d5c64b599bc08709a1a
Dnv14 <diego.navarro.140706@gmail.com>
2026-05-18 15:13:42 -0700

    infraestructura

---
+++ Infraestructura/src/main/java/com/mycompany/infraestructura/Infraestructura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.infraestructura;
+
+/**
+ *
+ * @author Diego
+ */
+public class Infraestructura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
